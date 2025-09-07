5b43361e7b7b468af7e72405f25423f1b4d5446a
Nick Leal <business.nleal@gmail.com>
2025-09-07 15:29:47 -0700

    first post link correction continued

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,17 +1,21 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import Layout from '../../components/layout';
+import styles from '../../components/layout.module.css';
+import utilStyles from '../../styles/utils.module.css';
  
 export default function FirstPost() {
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <title>First Post</title>
       </Head>
       <h1>First Post</h1>
+      <section className={utilStyles.headingMd}>
+      <p>This is my first post.</p>
+      </section>
       <h2>
         <Link href="/">← Back to home</Link>
       </h2>
-    </>
+    </div>
   );
 }
